5d850c93a88b82e901af0e5168804c74d30532f6
Nathan Eloe <powerofazure@gmail.com>
2014-10-22 15:02:44 -0500

    some minor cleanups

--- src/ngx_http_zmq_module.c
+++ src/ngx_http_zmq_module.c
@@ -143,118 +143,93 @@ ngx_module_t  ngx_http_zmq_module = {
 static ngx_int_t
 ngx_http_zmq_handler(ngx_http_request_t *r)
 {
+  ngx_int_t    rc;
+  ngx_int_t mlen = 0;
+  ngx_buf_t    *b = ngx_pcalloc(r->pool, sizeof(ngx_buf_t));
+  ngx_chain_t   out;
+  ngx_chain_t * c1;
+  unsigned char *string, *input, *it;
 
-    ngx_int_t    rc;
-    ngx_buf_t    *b;
-    ngx_chain_t   out;
-    unsigned char *string;
-    ngx_int_t mlen = 0;
-
-    // Get access to the module config variables
-    ngx_http_zmq_loc_conf_t  *zmq_config;
-    zmq_config = ngx_http_get_module_loc_conf(r, ngx_http_zmq_module);
+  /* Get access to the module config variables */
+  ngx_http_zmq_loc_conf_t  *zmq_config = ngx_http_get_module_loc_conf(r, ngx_http_zmq_module);
+  
 #if DEBUG
-    fprintf(stderr, "context: %ld\n", (long)zmq_config->ctx);
+  fprintf(stderr, "context: %ld\n", (long)zmq_config->ctx);
+  fprintf(stderr, "rb: %ld\n", (long)r->request_body);
 #endif
+  
 /* --------------- BEGIN MAGIC ------------------------- */
+  /* If there's an empty body, it's a bad request */
+  if (r->headers_in.content_length_n <= 0)
+  {
+    r->headers_out.status = NGX_HTTP_BAD_REQUEST;
+    r->header_only = 1;
+    r->headers_out.content_length_n = 0;
+    return ngx_http_send_header(r);
+  }
+  
+  it = input = ngx_pcalloc(r->pool, r->headers_in.content_length_n+1);
+  /* get the message? */
+  
+  for (c1 = r->request_body->bufs; c1; c1 = c1 -> next)
+    it = ngx_copy(it, c1->buf->pos, (c1->buf->last - c1->buf->pos));
 #if DEBUG
-    /*fprintf(stderr, "request body: %ld\n", (long) r->request_body);
-    fprintf(stderr, "content length: %d\n", (int) r->headers_in.content_length_n);
-    char * req = ngx_pcalloc(r->pool, (int)r->request_length+1);
-    ngx_memcpy(req, r->request_start, (int)r->request_length);
-    fprintf(stderr, "request: %s\n", req);*/
-    fprintf(stderr, "rb: %ld\n", (long)r->request_body);
+  fprintf(stderr, "Attempt at getting message body: %s\n", (char*)input);
 #endif
-    /* If there's an empty body, it's a bad request */
-    if (r->headers_in.content_length_n <= 0)
-    {
-      r->headers_out.status = NGX_HTTP_BAD_REQUEST;
-      r->header_only = 1;
-      r->headers_out.content_length_n = 0;
-      return ngx_http_send_header(r);
-    }
-    unsigned char *input;
-    input = ngx_pcalloc(r->pool, r->headers_in.content_length_n+1);
-    unsigned char *it = input;
-    /* get the message? */
-    ngx_chain_t * c1;
-    for (c1 = r->request_body->bufs; c1; c1 = c1 -> next)
-    {
-      ngx_memcpy(it, c1->buf->pos, (c1->buf->last - c1->buf->pos));
-      it += (c1->buf->last - c1->buf->pos);
-    }
+  
+  /* ----------- ZMQ LOOPY THING -------------- */
+  void* sock = zmq_socket(zmq_config->ctx, ZMQ_REQ);
+  char* endpt = ngx_pcalloc(r->pool, zmq_config->zmq_endpoint.len+1);
+  zmq_msg_t msg;
+  ngx_memcpy(endpt, zmq_config->zmq_endpoint.data, zmq_config->zmq_endpoint.len);
+  zmq_connect(sock, endpt);
+  zmq_send(sock, input , (int)r->headers_in.content_length_n, 0);
+  zmq_msg_init(&msg);
+  zmq_msg_recv(&msg, sock, 0);
+  mlen = zmq_msg_size(&msg);
+  string = ngx_pcalloc(r->pool, mlen+1);
+  ngx_memcpy(string, zmq_msg_data(&msg), mlen);
+  
 #if DEBUG
-    fprintf(stderr, "Attempt at getting message body: %s\n", (char*)input);
+  fprintf(stderr, "Got a reply, it is: %s with length %d\n", (char*)zmq_msg_data(&msg), (int)mlen);
+  fprintf(stderr, "sending back: %s\n", (char*)string);
 #endif
-    /* ----------- ZMQ LOOPY THING -------------- */
-    void* sock = zmq_socket(zmq_config->ctx, ZMQ_REQ);
-    char* endpt = ngx_pcalloc(r->pool, zmq_config->zmq_endpoint.len+1);
-    zmq_msg_t msg;
-    ngx_memcpy(endpt, zmq_config->zmq_endpoint.data, zmq_config->zmq_endpoint.len);
-    zmq_connect(sock, endpt);
-    zmq_send(sock, input , (int)r->headers_in.content_length_n, 0);
-    zmq_msg_init(&msg);
-    zmq_msg_recv(&msg, sock, 0);
-    mlen = zmq_msg_size(&msg);
-    string = ngx_pcalloc(r->pool, mlen+1);
-    ngx_memcpy(string, zmq_msg_data(&msg), mlen);
-    
-#if DEBUG
-    fprintf(stderr, "Got a reply, it is: %s with length %d\n", (char*)zmq_msg_data(&msg), (int)mlen);
-    fprintf(stderr, "sending back: %s\n", (char*)string);
-#endif
-    zmq_msg_close(&msg);
-    zmq_close(sock);
-    /* ----------- END ZMQ LOOPY THING -----------*/
+  zmq_msg_close(&msg);
+  zmq_close(sock);
+  /* ----------- END ZMQ LOOPY THING -----------*/
 
 /* --------------- END MAGIC ------------------------- */
-    
-    rc = ngx_http_discard_request_body(r);
+  
+  rc = ngx_http_discard_request_body(r);
 
-    r->headers_out.status = NGX_HTTP_OK;
-    r->headers_out.content_length_n = mlen;
-    r->headers_out.content_type.len = sizeof("text/plain") - 1;
-    r->headers_out.content_type.data = (u_char *) "text/plain";
+  r->headers_out.status = NGX_HTTP_OK;
+  r->headers_out.content_length_n = mlen;
+  r->headers_out.content_type.len = sizeof("text/plain") - 1;
+  r->headers_out.content_type.data = (u_char *) "text/plain";
 
-    if (r->method == NGX_HTTP_HEAD) {
-        rc = ngx_http_send_header(r);
+  if (!b)
+  {
+      ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "Failed to allocate response buffer.");
 
-        if (rc == NGX_ERROR || rc > NGX_OK || r->header_only) {
-            return rc;
-        }
-    }
+      return NGX_HTTP_INTERNAL_SERVER_ERROR;
+  }
 
-    b = ngx_pcalloc(r->pool, sizeof(ngx_buf_t));
+  out.buf = b;
+  out.next = NULL;
+  ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "zmq_endpoint: %s", zmq_config->zmq_endpoint.data);
+  b->pos = string; /* first position in memory of the data */
+  b->last = string + mlen; /* last position */
 
-    if (b == NULL) {
-        ngx_log_error(NGX_LOG_ERR, r->connection->log, 0,
-                      "Failed to allocate response buffer.");
+  b->memory = 1; /* content is in read-only memory */
+  /* (i.e., filters should copy it rather than rewrite in place) */
 
-        return NGX_HTTP_INTERNAL_SERVER_ERROR;
-    }
+  b->last_buf = 1; /* there will be no more buffers in the request */
 
-    out.buf = b;
-    out.next = NULL;
+  rc = ngx_http_send_header(r);
+  if (rc == NGX_ERROR || rc > NGX_OK || r->header_only)
+      return rc;
 
-    //string = ngx_palloc(r->pool, zmq_config->zmq_endpoint.len);
-    ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "zmq_endpoint: %s", zmq_config->zmq_endpoint.data);
-    b->pos = string; /* first position in memory of the data */
-    b->last = string + mlen; /* last position */
-
-    b->memory = 1; /* content is in read-only memory */
-    /* (i.e., filters should copy it rather than rewrite in place) */
-
-    b->last_buf = 1; /* there will be no more buffers in the request */
-
-    rc = ngx_http_send_header(r);
-    if (rc == NGX_ERROR || rc > NGX_OK || r->header_only) {
-        return rc;
-    }
-
-    ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "GOT THIS FAR!");
-
-
-    return ngx_http_output_filter(r, &out);
+  return ngx_http_output_filter(r, &out);
 
 }
 
